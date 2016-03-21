3d7dd41d56988ddb25d4fc655d1f3d6cca0a6f93
thergbway <thergbworld@gmail.com>
2016-03-21 05:09:56 +0300

    Added support for PUT /orders/{orderId} method for order updating

--- src/main/java/com/ruber/controller/OrdersController.java
+++ src/main/java/com/ruber/controller/OrdersController.java
@@ -1,10 +1,19 @@
 package com.ruber.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.github.fge.jackson.JsonLoader;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
 import com.ruber.controller.dto.AddOrderRequest;
 import com.ruber.controller.dto.GetOrderResponse;
 import com.ruber.controller.dto.OrderPreview;
+import com.ruber.dao.entity.OrderStatus;
 import com.ruber.service.OrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,14 +21,33 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
 @RestController
 @RequestMapping(OrdersController.PATH)
 public class OrdersController {
     public static final String PATH = "/orders";
+
+    @Value("WEB-INF/schema/orderUpdateSchema.json")//fixme can be removed because it needs only once in post construct
+    private Resource orderUpdateSchemaFile;
+
+    private JsonSchema orderUpdateSchema;
+
+    @PostConstruct
+    private void postConstruct() throws Exception {
+        JsonNode jsonNode = JsonLoader.fromFile(orderUpdateSchemaFile.getFile());
+
+        ArrayNode statusEnums = (ArrayNode) (jsonNode.get("properties").get("status").get("enum"));
+        for (OrderStatus orderStatus : OrderStatus.values()) {
+            statusEnums.add(orderStatus.toString());
+        }
+
+        orderUpdateSchema = JsonSchemaFactory.byDefault().getJsonSchema(jsonNode);
+    }
 
     @Autowired
     private OrdersService ordersService;
@@ -56,5 +84,28 @@ public class OrdersController {
         @PathVariable("id") Integer orderId) {
 
         return ordersService.getOrder(accessToken, orderId);
+    }
+
+    @RequestMapping(value = "/{id}", method = PUT)
+    @ResponseStatus(HttpStatus.OK)
+    public void updateOrder(
+        @RequestParam(value = "access_token", required = true) String accessToken,
+        @PathVariable("id") Integer orderId,
+        @RequestBody(required = true) JsonNode updateInfo
+    ) {
+        validateUpdateOrderJsonNode(updateInfo);
+
+        ordersService.updateOrder(accessToken, orderId, updateInfo);
+    }
+
+    private void validateUpdateOrderJsonNode(JsonNode node) {
+        try {
+            if (!orderUpdateSchema.validate(node).isSuccess()) {
+                throw new RuntimeException("Invalid Json");
+            }
+        } catch (ProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
--- src/main/java/com/ruber/service/OrdersService.java
+++ src/main/java/com/ruber/service/OrdersService.java
@@ -1,17 +1,21 @@
 package com.ruber.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.base.Function;
 import com.ruber.controller.dto.AddOrderRequest;
 import com.ruber.controller.dto.GetOrderResponse;
 import com.ruber.controller.dto.OrderPreview;
 import com.ruber.dao.OrderDAO;
 import com.ruber.dao.UserDAO;
-import com.ruber.dao.entity.Order;
-import com.ruber.dao.entity.User;
+import com.ruber.dao.entity.*;
 import com.ruber.util.TimeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -81,5 +85,138 @@ public class OrdersService {
             .stream()
             .map(OrderPreview::buildFromEntity)
             .collect(Collectors.toList());
+    }
+
+    public void updateOrder(String accessToken, Integer orderId, JsonNode updateInfo) {//fixme refactor: create several methods
+        if (!ruberTokensService.isValidToken(accessToken)) {
+            throw new RuntimeException("Invalid access token");
+        }
+
+        //TODO it is better to create a named query that gets order by Id for the specified user
+        User user = userDAO.getByRuberToken(accessToken);
+
+        List<Order> orders = user
+            .getOrders()
+            .stream()
+            .filter(order -> order.getId().equals(orderId))
+            .collect(Collectors.toList());
+
+        if (orders.size() == 0)
+            throw new RuntimeException(String.format("Order with id = %d for this user does not exist", orderId));
+
+        Order order = orders.get(0);
+
+        Function<JsonNode, Long> getAsLongOrNull = (JsonNode longNode) ->
+            longNode.isNull() ? null : longNode.asLong();
+
+        Function<JsonNode, Integer> getAsIntegerOrNull = (JsonNode integerNode) ->
+            integerNode.isNull() ? null : integerNode.asInt();
+
+        Function<JsonNode, String> getAsTextIfNotNull = (JsonNode textNode) ->
+            textNode == null ? null : textNode.asText();
+
+        if (updateInfo.has("title"))
+            order.setTitle(updateInfo.get("title").asText());
+
+        if (updateInfo.has("description"))
+            order.setDescription(updateInfo.get("description").asText());
+
+        if (updateInfo.has("status"))
+            order.setStatus(OrderStatus.valueOf(updateInfo.get("status").asText()));
+
+        if (updateInfo.has("deadline_timestamp"))
+            order.setDeadlineTimestamp(getAsLongOrNull.apply(updateInfo.get("deadline_timestamp")));
+
+        if (updateInfo.has("discount")) {
+            JsonNode discountInfo = updateInfo.get("discount");
+
+            if (discountInfo.isNull()) {
+                order.setDiscount(null);
+            } else {
+                String discountTitle = getAsTextIfNotNull.apply(discountInfo.get("title"));
+                String discountDescription = getAsTextIfNotNull.apply(discountInfo.get("description"));
+                String discountThumbPhoto = getAsTextIfNotNull.apply(discountInfo.get("thumb_photo"));
+                String discountCostAsText = getAsTextIfNotNull.apply(discountInfo.get("cost"));
+
+                BigDecimal discountCost =
+                    discountCostAsText != null ? new BigDecimal(discountCostAsText).setScale(2).negate() : null;
+
+                try {
+                    Discount discount = order.getDiscount();
+                    if (discount != null) {
+                        if (discountTitle != null)
+                            discount.setTitle(discountTitle);
+
+                        if (discountDescription != null)
+                            discount.setDescription(discountDescription);
+
+                        if (discountThumbPhoto != null)
+                            discount.setThumbPhoto(new URL(discountThumbPhoto));
+
+                        if (discountCost != null)
+                            discount.setCost(discountCost);
+                    } else {
+                        if (discountTitle == null ||
+                            discountDescription == null ||
+                            discountThumbPhoto == null ||
+                            discountCost == null)
+                            throw new RuntimeException("Not enough arguments were specified for new Discount");
+
+                        discount = new Discount(
+                            null,
+                            discountTitle,
+                            discountDescription,
+                            new URL(discountThumbPhoto),
+                            discountCost);
+
+                        order.setDiscount(discount);
+                    }
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        if (updateInfo.has("shipment")) {
+            JsonNode shipmentInfo = updateInfo.get("shipment");
+            if (shipmentInfo.isNull()) {
+                order.setShipment(null);
+            } else {
+                String shipmentAddress = getAsTextIfNotNull.apply(shipmentInfo.get("address"));
+                String shipmentCostAsText = getAsTextIfNotNull.apply(shipmentInfo.get("cost"));
+                BigDecimal shipmentCost =
+                    shipmentCostAsText != null ? new BigDecimal(shipmentCostAsText).setScale(2) : null;
+
+                Shipment shipment = order.getShipment();
+                if (shipment != null) {
+                    if (shipmentAddress != null)
+                        shipment.setAddress(shipmentAddress);
+
+                    if (shipmentCost != null)
+                        shipment.setCost(shipmentCost);
+                } else {
+                    if (shipmentAddress == null || shipmentCost == null)
+                        throw new RuntimeException("Not enough arguments were specified for new Shipment");
+
+                    shipment = new Shipment(null, shipmentAddress, shipmentCost);
+
+                    order.setShipment(shipment);
+                }
+            }
+        }
+
+        JsonNode customerInfo = updateInfo.get("customer");
+        if (customerInfo != null) {
+            Customer customer = order.getCustomer();
+
+            if (customerInfo.has("name"))
+                customer.setName(customerInfo.get("name").asText());
+
+            if (customerInfo.has("phone"))
+                customer.setPhone(customerInfo.get("phone").asText());
+
+            if (customerInfo.has("vk_id"))
+                customer.setVkId(getAsIntegerOrNull.apply(customerInfo.get("vk_id")));
+        }
     }
 }
