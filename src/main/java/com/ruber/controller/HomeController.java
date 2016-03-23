2ab59050c51ece576c9ecc44f889227852eec15a
thergbway <thergbworld@gmail.com>
2016-03-23 03:19:49 +0300

    Removed test logging from Home controller

--- src/main/java/com/ruber/controller/HomeController.java
+++ src/main/java/com/ruber/controller/HomeController.java
@@ -1,8 +1,6 @@
 package com.ruber.controller;
 
 import com.ruber.service.FillDatabaseService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,14 +12,12 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Controller
 public class HomeController {
     private static AtomicInteger nextId = new AtomicInteger(1);
-    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private FillDatabaseService fillDatabaseService;
 
     @RequestMapping({"/", "/home"})
     public String getHomePage(Model model) {
-        logger.error("new call");
         model.addAttribute("id", nextId.getAndIncrement());
 
         return "home";
